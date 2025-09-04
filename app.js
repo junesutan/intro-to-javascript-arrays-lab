971b4a9d65038248a13fb11e7603a3901416852f
junesutan <juneowns@gmail.com>
2025-09-04 15:43:22 +0800

    until ex 12

--- app.js
+++ app.js
@@ -37,18 +37,62 @@ console.log('Exercise 5 result:', foods);
 
 
 //EXERCISE 6 -- replace 
-
 foods.splice(1,1,"sushi","cupcake")
 console.log('Exercise 6 result:', foods);
 
 //EXERCISE 7 -- slice() to copy a part of an array (or string).
-
 const yummy = foods.slice(1, 3)
-
 console.log('Exercise 7 result:', yummy);
 
 //EXERCISE 8 -- indexOf() to search 
-
 const soyIdx = foods.indexOf("tofu")
-
 console.log('Exercise 8 result:', soyIdx);
+
+//EXERCISE 9 -- join()
+const allFoods = foods.join(" -> ");
+console.log('Exercise 9 result:', allFoods);
+
+//EXERCISE 10 -- includes()
+const hasSoup = foods.includes("soup")
+console.log('Exercise 10 result:', hasSoup);
+
+
+//EXERCISE 11 -- for loop 
+
+const odds = []
+
+const nums = [100, 5, 23, 15, 21, 72, 9, 45, 66, 7, 81, 90];
+
+for (const num of nums) {
+  if (num % 2 !== 0) {              // I copied this 
+    odds.push(num);                 // I copied this 
+  }
+}
+
+console.log('Exercise 11 result:', odds);
+
+
+//EXERCISE 12 -- fizzbuzz 
+
+const fizz = []
+const buzz = []
+const fizzbuzz = []
+
+for (const num of nums) {
+  if (num % 3 === 0) {              // I copied this 
+    fizz.push(num);                 // I copied this 
+  }
+  if (num % 5 === 0) {              // I copied this 
+    buzz.push(num);  
+  }
+  if (num % 3 === 0 && num % 5 === 0) {
+    fizzbuzz.push(num);
+  }
+}
+
+console.log('Exercise 12 Results:');
+console.log('  fizz:', fizz);
+console.log('  buzz:', buzz);
+console.log('  fizzbuzz:', fizzbuzz);
+
+
