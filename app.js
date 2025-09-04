79ddca8d6672afc43fa02d612c526228e207bba1
junesutan <juneowns@gmail.com>
2025-09-04 12:29:24 +0800

    ex 1-4

---
+++ app.js
@@ -0,0 +1,31 @@
+//EXERCISE 1
+
+const foods = [];  
+
+console.log('Exercise 1 result:', foods);
+
+
+//EXERCISE 2
+
+foods.push("pizza")
+foods.push("cheeseburger")
+
+console.log('Exercise 2 result:', foods);
+
+
+//EXERCISE 3
+
+foods.unshift("taco")
+
+console.log('Exercise 3 result:', foods);
+
+//EXERCISE 4
+
+const favFood = foods[1]
+
+console.log('Exercise 4 result:', favFood);
+
+
+//EXERCISE 5
+
+console.log(foods)
